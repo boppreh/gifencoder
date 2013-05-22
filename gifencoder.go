10c667c2d27cdfd66df8cecc019b0a3c6c4cae40
BoppreH <boppreh@gmail.com>
2013-05-21 22:27:25 -0300

    Add working prototype

---
+++ gifencoder.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+    "fmt" 
+    //"image"
+    //"image/gif"
+    "os"
+    //"bufio"
+    "io/ioutil"
+    //"io"
+    "compress/lzw"
+    "bytes"
+)
+
+func main() {
+    file, _ := os.Open("image2.gif")
+    fileBytes, _ := ioutil.ReadAll(file)
+
+    imageSize := int(fileBytes[0x320])
+    compressedImageBytes := fileBytes[0x321:0x321 + imageSize]
+
+    compressedBuffer := bytes.NewBuffer(compressedImageBytes)
+    lzwr := lzw.NewReader(compressedBuffer, lzw.LSB, int(8))
+    imageBytes, _ := ioutil.ReadAll(lzwr)
+
+    for i := 0; i < imageSize; i++ {
+        compressedImageBytes[i] = 0x00
+    }
+    fmt.Println(imageBytes)
+
+    compressedBuffer.Reset()
+    lzww := lzw.NewWriter(compressedBuffer, lzw.LSB, int(8))
+    lzww.Write(imageBytes)
+
+    for i := 0; i < imageSize; i++ {
+        fileBytes[0x321 + i] = compressedImageBytes[i]
+    }
+    //fmt.Println(lzwr)
+    ioutil.WriteFile("image2_new.gif", fileBytes, 0777)
+}
