7103be4d73863e69f9838b449b931ac2f76d21dc
BoppreH <boppreh@gmail.com>
2013-05-22 02:32:15 -0300

    Use GIF structure from standard library

--- gifencoder.go
+++ gifencoder.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-    //"fmt" 
     "image"
     "image/color"
-    //"image/gif"
+    "image/gif"
     "os"
     "bufio"
     "io"
@@ -13,68 +12,69 @@ import (
     "math"
 )
 
-func writeHeader(w *bufio.Writer, m *image.Paletted) {
-    w.Write([]byte("GIF89a"))
+func writeHeader(w *bufio.Writer, image *gif.GIF) {
+    w.Write([]uint8("GIF89a"))
 
-    b := m.Bounds()
-    w.WriteByte(byte(b.Max.X % 255)) // Paletted width, LSB.
-    w.WriteByte(byte(b.Max.X / 255)) // Paletted width, MSB.
-    w.WriteByte(byte(b.Max.Y % 255)) // Paletted height, LSB.
-    w.WriteByte(byte(b.Max.Y / 255)) // Paletted height, MSB.
+    b := image.Image[0].Bounds()
+    w.WriteByte(uint8(b.Max.X % 255)) // Paletted width, LSB.
+    w.WriteByte(uint8(b.Max.X / 255)) // Paletted width, MSB.
+    w.WriteByte(uint8(b.Max.Y % 255)) // Paletted height, LSB.
+    w.WriteByte(uint8(b.Max.Y / 255)) // Paletted height, MSB.
 
-    w.WriteByte(byte(0xF7)) // GCT follows for 256 colors with resolution
+    w.WriteByte(uint8(0xF7)) // GCT follows for 256 colors with resolution
                                  // 3 x 8 bits/primary
-    w.WriteByte(byte(0x00)) // Background color.
-    w.WriteByte(byte(0x00)) // Default pixel aspect ratio.
+    w.WriteByte(uint8(0x00)) // Background color.
+    w.WriteByte(uint8(0x00)) // Default pixel aspect ratio.
 
     // Global Color Table.
-    for i := 0; i < len(m.Palette); i++ {
-        r, g, b, _ := m.Palette[i].RGBA()
-        w.WriteByte(byte(r))
-        w.WriteByte(byte(g))
-        w.WriteByte(byte(b))
+    palette := image.Image[0].Palette
+    for _, c := range palette {
+        r, g, b, _ := c.RGBA()
+        w.WriteByte(uint8(r))
+        w.WriteByte(uint8(g))
+        w.WriteByte(uint8(b))
     }
 
-    w.WriteByte(byte(0x21)) // Application Extension block.
-    w.WriteByte(byte(0xFF)) // Application Extension block (cont).
-    w.WriteByte(byte(0x0B)) // Next 11 bytes are Application Extension.
-    w.Write([]byte("NETSCAPE2.0")) // 8 Character application name.
-    w.WriteByte(byte(0x03)) // 3 more bytes of Application Extension.
-    w.WriteByte(byte(0x01)) // Data sub-block index (always 1).
-    w.WriteByte(byte(0xFF)) // Number of repetitions, LSB.
-    w.WriteByte(byte(0xFF)) // Number of repetitions, MSB.
-    w.WriteByte(byte(0x00)) // End of Application Extension block.
+    w.WriteByte(uint8(0x21)) // Application Extension block.
+    w.WriteByte(uint8(0xFF)) // Application Extension block (cont).
+    w.WriteByte(uint8(0x0B)) // Next 11 bytes are Application Extension.
+    w.Write([]uint8("NETSCAPE2.0")) // 8 Character application name.
+    w.WriteByte(uint8(0x03)) // 3 more bytes of Application Extension.
+    w.WriteByte(uint8(0x01)) // Data sub-block index (always 1).
+    w.WriteByte(uint8(0xFF)) // Number of repetitions, LSB.
+    w.WriteByte(uint8(0xFF)) // Number of repetitions, MSB.
+    w.WriteByte(uint8(0x00)) // End of Application Extension block.
 }
 
-func writeFrameHeader(w *bufio.Writer, m *image.Paletted) {
-    w.WriteByte(byte(0x21)) // Start of Graphic Control Extension.
-    w.WriteByte(byte(0xF9)) // Start of Graphic Control Extension (cont).
-    w.WriteByte(byte(0x04)) // 4 more bytes of GCE.
+func writeFrameHeader(w *bufio.Writer, m *image.Paletted, delay int) {
+    w.WriteByte(uint8(0x21)) // Start of Graphic Control Extension.
+    w.WriteByte(uint8(0xF9)) // Start of Graphic Control Extension (cont).
+    w.WriteByte(uint8(0x04)) // 4 more bytes of GCE.
 
-    w.WriteByte(byte(0x08)) // There is no transparent pixel.
-    w.WriteByte(byte(0x10)) // Animation delay, in centiseconds, LSB.
-    w.WriteByte(byte(0x00)) // Animation delay, in centiseconds, MSB.
-    w.WriteByte(byte(0x00)) // Transparent color #, if we were using.
-    w.WriteByte(byte(0x00)) // End of Application Extension data.
+    w.WriteByte(uint8(0x08)) // There is no transparent pixel.
+    w.WriteByte(uint8(delay % 0xFF)) // Animation delay, in centiseconds, LSB.
+    w.WriteByte(uint8(delay / 0xFF)) // Animation delay, in centiseconds, MSB.
+    w.WriteByte(uint8(0x00)) // Transparent color #, if we were using.
+    w.WriteByte(uint8(0x00)) // End of Application Extension data.
 
-    w.WriteByte(byte(0x2C)) // Start of Paletted Descriptor.
+    w.WriteByte(uint8(0x2C)) // Start of Paletted Descriptor.
 
     b := m.Bounds()
-    w.WriteByte(byte(b.Min.X % 255)) // Minimum x (can be > 0), LSB.
-    w.WriteByte(byte(b.Min.X / 255)) // Minimum x (can be > 0), MSB.
-    w.WriteByte(byte(b.Min.Y % 255)) // Minimum y (can be > 0), LSB.
-    w.WriteByte(byte(b.Min.Y / 255)) // Minimum y (can be > 0), MSB.
+    w.WriteByte(uint8(b.Min.X % 255)) // Minimum x (can be > 0), LSB.
+    w.WriteByte(uint8(b.Min.X / 255)) // Minimum x (can be > 0), MSB.
+    w.WriteByte(uint8(b.Min.Y % 255)) // Minimum y (can be > 0), LSB.
+    w.WriteByte(uint8(b.Min.Y / 255)) // Minimum y (can be > 0), MSB.
 
-    w.WriteByte(byte(b.Max.X % 255)) // Frame width, LSB.
-    w.WriteByte(byte(b.Max.X / 255)) // Frame width, MSB.
-    w.WriteByte(byte(b.Max.Y % 255)) // Frame height, LSB.
-    w.WriteByte(byte(b.Max.Y / 255)) // Frame height, MSB.
+    w.WriteByte(uint8(b.Max.X % 255)) // Frame width, LSB.
+    w.WriteByte(uint8(b.Max.X / 255)) // Frame width, MSB.
+    w.WriteByte(uint8(b.Max.Y % 255)) // Frame height, LSB.
+    w.WriteByte(uint8(b.Max.Y / 255)) // Frame height, MSB.
 
-    w.WriteByte(byte(0x00)) // No local color table.
+    w.WriteByte(uint8(0x00)) // No local color table.
 }
 
 func compressImage(m *image.Paletted) *bytes.Buffer {
-    compressedImageBuffer := bytes.NewBuffer(make([]byte, 0, 255))
+    compressedImageBuffer := bytes.NewBuffer(make([]uint8, 0, 255))
     lzww := lzw.NewWriter(compressedImageBuffer, lzw.LSB, int(8))
     lzww.Write(m.Pix)
     lzww.Close()
@@ -82,10 +82,10 @@ func compressImage(m *image.Paletted) *bytes.Buffer {
     return compressedImageBuffer
 }
 
-func writeFrame(w *bufio.Writer, m *image.Paletted) {
-    writeFrameHeader(w, m)
+func writeFrame(w *bufio.Writer, m *image.Paletted, delay int) {
+    writeFrameHeader(w, m, delay)
 
-    w.WriteByte(byte(0x08)) // Start of LZW with minimum code size 8.
+    w.WriteByte(uint8(0x08)) // Start of LZW with minimum code size 8.
 
     compressedImage := compressImage(m)
 
@@ -95,7 +95,7 @@ func writeFrame(w *bufio.Writer, m *image.Paletted) {
     for bytesRemaining > 0 {
         if bytesSoFar == 0 {
             blockSize := math.Min(maxBlockSize, float64(bytesRemaining))
-            w.WriteByte(byte(blockSize))
+            w.WriteByte(uint8(blockSize))
         }
 
         b, _ :=  compressedImage.ReadByte()
@@ -105,19 +105,22 @@ func writeFrame(w *bufio.Writer, m *image.Paletted) {
         bytesRemaining--
     }
 
-    w.WriteByte(byte(0x00)) // End of LZW data.
+    w.WriteByte(uint8(0x00)) // End of LZW data.
 }
 
 func Encode(w io.Writer, m *image.Paletted) error {
-    return EncodeAll(w, []*image.Paletted{m})
+    animation := gif.GIF{[]*image.Paletted{m}, []int{0}, 0}
+    return EncodeAll(w, &animation)
 }
 
-func EncodeAll(w io.Writer, images []*image.Paletted) error {
+func EncodeAll(w io.Writer, animation *gif.GIF) error {
     buffer := bufio.NewWriter(w)
 
-    writeHeader(buffer, images[0])
-    for _, m := range images {
-        writeFrame(buffer, m)
+    writeHeader(buffer, animation)
+    for i, _ := range animation.Image {
+        image := animation.Image[i]
+        delay := animation.Delay[i]
+        writeFrame(buffer, image, delay)
     }
     buffer.WriteByte(';')
     buffer.Flush()
@@ -128,8 +131,8 @@ func EncodeAll(w io.Writer, images []*image.Paletted) error {
 func main() {
     p := make([]color.Color, 256)
     for i := 0; i < 256; i++ {
-        c := byte((i / 16) ^ (i % 16))
-        p[i] = color.RGBA{c, c, c, 0xFF}
+        c := uint8((i / 16) ^ (i % 16))
+        p[i] = color.RGBA{c, c * uint8(i), c ^ uint8(i), 0xFF}
     }
 
     m1 := image.NewPaletted(image.Rect(0, 0, 100, 100), p)
@@ -140,5 +143,7 @@ func main() {
     }
 
     file, _ := os.Create("new_image.gif")
-    EncodeAll(file, []*image.Paletted{m1})
+
+    animation := gif.GIF{[]*image.Paletted{m1}, []int{0}, 0}
+    EncodeAll(file, &animation)
 }
